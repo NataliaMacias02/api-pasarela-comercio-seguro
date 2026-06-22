e5900825e4b72ba599c30196ff9a2e6b7a7c51a7
Naty Macias <st4441@utr.edu.mx>
2026-06-22 09:19:13 -0600

    Base Configuration

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("API Pasarela de Comercio Seguro");
+const express = require("express");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+
+const PORT = 5100;
+
+app.listen(PORT, () => {
+  console.log("Hello World");
+  console.log(`Server running on port ${PORT}`);
+});
